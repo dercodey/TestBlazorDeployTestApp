b364da118d7221097c56cf871d8907760c2cd748
Lane, Derek <dg1an3@2nd-messenger.com>
2021-11-03 15:52:21 -0500

    adding swagger endpoint

--- BlazorDeployTestApp/Server/Startup.cs
+++ BlazorDeployTestApp/Server/Startup.cs
@@ -22,9 +22,9 @@ namespace BlazorDeployTestApp.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +41,9 @@ namespace BlazorDeployTestApp.Server
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
             app.UseBlazorFrameworkFiles();
